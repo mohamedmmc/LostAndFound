36f582687ee52ae5699313dc91ccb81da77ae3c4
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2021-11-02 01:06:30 +0100

    Creation projet

---
+++ Lost&Found/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lost&Found
+//
+//  Created by Mohanmed Melek Chtourou on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func CreateAccountB(_ sender: UIButton) {
+        performSegue(withIdentifier: "CreateAccount", sender: nil)
+    }
+}
+
